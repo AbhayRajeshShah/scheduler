e1f662e2e5285daffdad845796daded504fe6983
Abhay <abhayrajeshshah@gmail.com>
2022-02-15 17:59:39 +0530

    Variable Change

--- components.js
+++ components.js
@@ -1,13 +1,13 @@
 const input_btn=(fun,parentid,i)=>{
     let functionstring = `${fun}('${fun}','${parentid}','${i}')`;
     let pholder="";
-    if(idname==="add"){
+    if(parentid==="add"){
          pholder = "Subject Name"
     }else{
          pholder="Chapter Name"
     }
     return `
-            <div class="add" id=${idname}>
+            <div class="add" id=${parentid}>
             <input type="text" placeholder=${pholder} name="sub" id='${fun}${i}'>
             <button class="new_sub"  onclick=${functionstring} >+</button>  
             </div>    
